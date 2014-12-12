a2194f040d4db7cf05ba0c9a5776d025407de3ad
PeterQLee <leep1995@gmail.com>
2014-12-11 20:42:48 -0400

    Added main c file \n don't have  alot of time to work on this rn... exams

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <GLUT/glut.h>
+
+
+void draw() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0, 1.0, 1.0);
+  glBegin(GL_LINES);
+  glVertex3f(0.25, 0.25, 0.0);
+  glVertex3f(0.75, 0.75, 0.0);
+  glEnd();
+  glFlush();
+}
+
+void initialize() {
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+void dostuff(int argc,char ** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(300, 300);
+  glutInitWindowPosition(150, 1500);
+  glutCreateWindow("Pythontest");
+  initialize();
+  glutDisplayFunc(draw);
+  glutMainLoop();
+  
+}
+int main(int argc, char** argv) {
+  dostuff(argc, argv);
+  return 0;
+}
