d80c2fd8488d671a9090117451b2118d4408c1ab
PeterQLee <leep1995@gmail.com>
2014-12-16 17:36:41 -0400

    worked on picture handling

--- src/instance.py
+++ src/instance.py
@@ -1,3 +1,4 @@
+import tools #not sure if this will be included
 
 class instance:
     def __init__(self, stackname):
@@ -5,12 +6,25 @@ class instance:
         self.li=[255,0,0,255,0,0,255,0,0,255,0,0,255,0,0,255,0,0,
                  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                  255,0,0,255,0,0,255,0,0,255,0,0,255,0,0,255,0,0]
-        self.colormap={"A":"#000000","B":"#444444"}
+        self.colormapold={"A":"#000000","B":"#444444"} #character range will be anywhere between chr(0) to chr(255), after that, additional characters will be created
+        self.lettermap="AABBAA"
+        self.height=3
+        self.width=2
+        self.colorsize=1
+        self.colormap={"A":tools.converthex("#000000"),"B":tools.converthex("#444444")}
         
         #will predetermine size, so can just be a singular list of values
-
-    def getPixmap(self):
+        
+    def getPixmap(self): #DEPRICATE
         return self.li
-    def getColormap(self):
-        return self.colormap
-    
+    def getColormap(self): #DEPRICATE
+        return self.colormapold
+    def getRGBMap(self):
+        RGBA=[0]*self.height*self.width*4
+        for y in range(self.height):
+            for x in range(self.width):
+                start=y*self.width*4+int(x/self.colorsize)*4
+                letterpos=y*self.width*self.colorsize+x*self.colorsize
+                RGBA[start:start+4]=self.colormap[self.lettermap[letterpos:letterpos+self.colorsize]] #check this
+        return RGBA       
+        
